6088f2f6f7440d496d9b695d96b7e1191dd09f8a
pengincoalition <kage2.kage22@gmail.com>
2022-05-27 20:49:58 +0900

    Change the file name of data.

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # ママ活勧誘DMコーパス
 
+## ダウンロード
+[Mama_katu_DM_corpus.txt](https://raw.githubusercontent.com/PenguinCabinet/mama-katu-DM-corpus/main/data.txt)
+
 ## 概要
 ママ活の勧誘のDMを集めてコーパスにしたものです。
 
-## 概要
+## 仕様
 * 一行に一つのママ活DMのテキストです
 * 改行は「__BR__」という記号に変換しています
 * 送り先ユーザー名は「__TOUSER__」という記号に変換しています
 * 送り元ユーザー名は「__FROMUSER__」という記号に変換しています
+
+## 追加方法
+1. in_conv.txtにママ活DMの内容を書き書き
+2. python conv.pyを実行
+3. Mama_katu_DM_corpus.txtに追加されている
--- conv.py
+++ conv.py
@@ -6,7 +6,7 @@ with open("in_conv.txt","r",encoding="utf-8") as f:
 text=text.replace("\n","__br__")
 
 out_text=""
-with open("data.txt","r",encoding="utf-8") as f:
+with open("Mama_katu_DM_corpus.txt","r",encoding="utf-8") as f:
     for line in f:
         if not line.isspace():
             out_text+=line
@@ -15,5 +15,5 @@ if out_text=="" or out_text[len(out_text)-1]=="\n":
     out_text+=text
 else:
     out_text+="\n"+text
-with open("data.txt","w",encoding="utf-8") as f:
+with open("Mama_katu_DM_corpus.txt","w",encoding="utf-8") as f:
     f.write(out_text)
